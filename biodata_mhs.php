bc56ab4b702eff02a00a1c8e75451d9713ce09af
phpcode.id <41561993+Myhermanto@users.noreply.github.com>
2025-09-10 13:39:26 +0700

    Create biodata_mhs.php

---
+++ biodata_mhs.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <title>Biodata Mahasiswa</title>
+</head>
+
+<body>
+    <h1>Biodata Mahasiswa</h1>
+    <form method="post" action="simpan_biodata.php">
+        Nama: <input type="text" name="nama"><br>
+        NIM: <input type="text" name="nim"><br>
+        Jurusan: <input type="text" name="jurusan"><br>
+        Kampus: <input type="text" name="kampus"><br>
+        Alamat: <input type="text" name="alamat"><br>
+        <input type="submit" value="Simpan">
+    </form>
+
+</body>
+
+</html>
